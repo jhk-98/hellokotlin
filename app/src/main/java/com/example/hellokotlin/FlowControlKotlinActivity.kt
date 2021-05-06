2fe04ad6f7f406118ff2603bbe08da2acd42bb46
JunHyuk <kkjun221@naver.com>
2021-05-06 14:59:27 +0900

    Flow Control in Kotlin

--- app/src/main/java/com/example/hellokotlin/FlowControlKotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/FlowControlKotlinActivity.kt
@@ -1,15 +1,36 @@
 package com.example.hellokotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class FlowControlKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_flowcontrol)
 
-        val btnFlowControlKotlin = findViewById<Button>(R.id.btnFlowControlKotlin)
+        val btnLD = findViewById<Button>(R.id.btnLD)
+        val fieldNumber = findViewById<EditText>(R.id.fieldNumber)
 
+        btnLD.setOnClickListener {
+            val number = fieldNumber.text.toString().toInt()
+
+            if (number % 2 == 0) {
+                Toast.makeText(
+                    applicationContext, "" + number + "은(는) 2의배수", Toast.LENGTH_SHORT).show()
+            } else if (number % 3 == 0) {
+                Toast.makeText(
+                    applicationContext, "" + number + "은(는) 3의배수", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    applicationContext,"" + number + "은(는)else", Toast.LENGTH_SHORT).show()
+            }
+            when (number) {
+                4,9 -> btnLD.text = "실행for"+number
+                else -> btnLD.text = "실행 for else"
+            }
+        }
     }
 }
